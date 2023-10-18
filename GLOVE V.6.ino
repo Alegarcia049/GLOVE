418a49d22d0c2b2d878bae2a555fb4c7d2973757
Alegarcia049 <alegarcia049@gmail.com>
2023-10-18 16:12:36 +0000

    r

--- GLOVE V.6.ino
+++ GLOVE V.6.ino
@@ -1,3 +1,28 @@
+int PUL1 = 36;   //Pulsador de a,b,c  ERROR
+int PUL2 = 39;   //Pulsador de d,e,f  ERROR
+int PUL5 = 34;   //Pulsador de g,h,i  ERROR
+int PUL11 = 35;   //Pulsador de j,k,l 
+int PUL3 = 32;   //Pulsador de m,n,ñ
+int PUL7 = 33;   //Pulsador de o,p,q
+int PUL8 = 25;   //Pulsador de r,s,t
+int PUL6 = 26;   //Pulsador de u,v,w  ERROR
+int PUL4 = 27;   //Pulsador de x,y,z
+int PUL10 = 14;  //Pulsador de space/send
+int PUL9 = 12;  //Pulsador de erase/delete
+int PUL12 = 13;  //Pulsador configurable
+
+int VIB1 = 23;
+int VIB2 = 22;
+int VIB3 = 21;
+int VIB4 = 19;
+int VIB5 = 18;
+int VIB6 = 5;
+int VIB7 = 17;
+int VIB8 = 16;
+int VIB9 = 4;
+
+int del = 100;
+
 #include <string.h>
 #include <iostream>
 #include "BluetoothSerial.h"
@@ -19,7 +44,7 @@ String device_name = "ESP32-BT-Slave";
 
 BluetoothSerial SerialBT;
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(VIB1, OUTPUT);
   pinMode(VIB2, OUTPUT);
   pinMode(VIB3, OUTPUT);
